b77e52200b2edd8568e426b73c4fc9760fc77e7e
sergadin <serg@msu.ru>
2018-01-25 12:05:44 +0300

    Change scripts permissions
    Travis test failed with 'permission denied' error.

--- .travis/setup_db_for_testing.sh
+++ .travis/setup_db_for_testing.sh
@@ -1,44 +1,33 @@
-#!/bin/sh -e
+#!/bin/bash -ev
 
 #
 # Creatre Oracle user and populate his schema with data for testing
 #
 
 $ORACLE_HOME/bin/sqlplus / as sysdba <<EOF
-create user scott identified by tiger
+CREATE USER scott IDENTIFIED BY tiger
 /
 
-grant connect to scott
-/
-
-grant resource to scott
-/
-
-quit
+GRANT connect, resource TO scott
 /
 EOF
 
 $ORACLE_HOME/bin/sqlplus scott/tiger <<EOF
-drop table realval
+DROP TABLE realval
 /
 
-create table realval (
-    r real,
-    n number,
-    ns number(*, 5),
-    nps number(10, 2),
-    i integer
+CREATE TABLE realval (
+    r REAL,
+    n NUMBER,
+    ns NUMBER(*, 5),
+    nps NUMBER(10, 2),
+    i INTEGER
 )
 /
 
-insert into realval values (17.17, 17.17, 17.17, 17.17, 17)
+INSERT INTO realval VALUES (17.17, 17.17, 17.17, 17.17, 17)
 /
 
-commit
-/
-
-quit
+COMMIT
 /
 EOF
-
-exit 0
