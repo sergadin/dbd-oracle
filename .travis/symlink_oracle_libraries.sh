98b42c46090bc8c6ac6f3120237f2219546e6e0f
sergadin <serg@msu.ru>
2018-01-25 16:04:41 +0300

    Travis setup: export LD_LIBRARY_PATH

--- .travis/symlink_oracle_libraries.sh
+++ .travis/symlink_oracle_libraries.sh
@@ -6,8 +6,6 @@ cd $ORACLE_HOME/lib
 ls -la
 
 sudo ln -s libclntsh.so.11.2 libclntsh.so
-sudo ln -s libocci.so.11.2 libocci.so
-sudo ln -s liboci.so.11.2 liboci.so
 for f in `ls ./*.so*`
 do
    sudo ln -s $ORACLE_HOME/lib/$f /opt/oracle/$f
